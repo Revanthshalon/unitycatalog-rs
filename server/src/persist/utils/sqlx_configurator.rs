f2da5d49cf3f3807f9b2881d3e4a058f39722f4a
Revanth Shalon Raj <revanth@Revanths-MacBook-Air.local>
2025-01-28 16:32:24 +0530

    Implementing SqlxConfigurator

--- server/src/persist/utils/sqlx_configurator.rs
+++ server/src/persist/utils/sqlx_configurator.rs
@@ -1,6 +1,13 @@
 #![allow(dead_code)]
+use super::super::models::DatabaseConfig;
 use sqlx::{Pool, Postgres};
 
 pub struct SqlxConfigurator {
     pub db_pool: Pool<Postgres>,
+}
+
+impl SqlxConfigurator {
+    pub fn new(_database_config: DatabaseConfig) -> Self {
+        todo!()
+    }
 }
