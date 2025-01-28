50185441abd82424bdc76ea7b9fde06b78a63f0d
Revanth Shalon Raj <revanth@Revanths-MacBook-Air.local>
2025-01-28 23:06:00 +0530

    main

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,13 @@
+use config::database_config::DatabaseConfig;
+
+mod config;
+mod errors;
 mod persist;
 
 fn main() {
-    println!("Hello, world!");
+    let app_config = DatabaseConfig::load_config();
+    match app_config {
+        Ok(config) => println!("{:#?}", config),
+        Err(e) => println!("{:#?}", e),
+    }
 }
