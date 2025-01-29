27be7c6c2176e19c299b20d9db112c28dd5f77fb
Revanth Shalon Raj <revanth@Revanths-MacBook-Air.local>
2025-01-29 12:19:48 +0530

    feat: Application Configuration

--- server/src/config/database_config.rs
+++ server/src/config/database_config.rs
@@ -1,11 +1,11 @@
 #![allow(dead_code)]
 
-use std::{env, sync::Arc};
+use std::{env, sync::Arc, time::Duration};
 
 use config::{Config, File, FileFormat};
 use serde::Deserialize;
 
-use crate::errors::UnityCatalogResult;
+use crate::errors::{UnityCatalogError, UnityCatalogResult};
 
 #[derive(Debug, Deserialize)]
 pub struct DatabaseConfig {
@@ -14,25 +14,55 @@ pub struct DatabaseConfig {
     password: String,
     host: String,
     port: u16,
+    database_name: String,
     max_idle_connections: u32,
     max_open_connections: u32,
     connection_timeout: u64,
 }
 
 impl DatabaseConfig {
-    pub fn load_config() -> UnityCatalogResult<Arc<Self>> {
+    pub fn load_config(path_option: Option<String>) -> UnityCatalogResult<Arc<Self>> {
         let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
+        let path = path_option.unwrap_or_else(|| "config/default".into());
 
         let config = Config::builder()
-            .add_source(File::new("config/default", FileFormat::Toml))
+            .add_source(File::new(&path, FileFormat::Toml))
             .add_source(
                 File::new(&format!("config/{}", run_mode), FileFormat::Toml).required(false),
             )
             .build()?;
 
         let database_config = config.try_deserialize()?;
-        env::remove_var("RUN_MODE");
         Ok(Arc::new(database_config))
+    }
+
+    pub fn get_connection_string(&self) -> UnityCatalogResult<String> {
+        match self.database_type.as_str() {
+            "postgres" => Ok(format!(
+                "postgres://{}:{}@{}:{}/{}",
+                self.username, self.password, self.host, self.port, self.database_name
+            )),
+            "mysql" => Ok(format!(
+                "mysql://{}:{}@{}:{}/{}",
+                self.username, self.password, self.host, self.port, self.database_name
+            )),
+            _ => Err(UnityCatalogError::Unsupported(format!(
+                "Unsupported Database: {}",
+                self.database_type
+            ))),
+        }
+    }
+
+    pub fn get_max_idle_connections(&self) -> u32 {
+        self.max_idle_connections
+    }
+
+    pub fn get_max_open_connections(&self) -> u32 {
+        self.max_open_connections
+    }
+
+    pub fn get_timeout(&self) -> Duration {
+        Duration::from_secs(self.connection_timeout)
     }
 }
 
@@ -42,7 +72,7 @@ mod tests {
 
     #[test]
     pub fn load_default_config_works() {
-        let config = DatabaseConfig::load_config();
+        let config = DatabaseConfig::load_config(None);
 
         assert!(config.is_ok());
 
--- server/src/config/mod.rs
+++ server/src/config/mod.rs
@@ -1,3 +1,14 @@
+#![allow(dead_code)]
 pub mod database_config;
 
 pub use self::database_config::DatabaseConfig;
+
+pub struct AppConfig {
+    pub database_config: DatabaseConfig,
+}
+
+impl AppConfig {
+    pub fn new(_path_option: Option<String>) -> Self {
+        todo!()
+    }
+}
