0d4c60e45f97b8664322606bfb7bc2852319f037
Revanth Shalon Raj <revanth@Revanths-MacBook-Air.local>
2025-01-29 12:21:28 +0530

    enhancement: UnityCatalogError
    - Unsupported Operations

--- server/src/errors/mod.rs
+++ server/src/errors/mod.rs
@@ -3,6 +3,7 @@ use config::ConfigError;
 #[derive(Debug)]
 pub enum UnityCatalogError {
     Config(ConfigError),
+    Unsupported(String),
 }
 
 pub type UnityCatalogResult<T> = Result<T, UnityCatalogError>;
@@ -11,6 +12,7 @@ impl std::fmt::Display for UnityCatalogError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             UnityCatalogError::Config(error) => write!(f, "Configuration Error: {error}"),
+            UnityCatalogError::Unsupported(message) => write!(f, "Unsupported Error: {message}"),
         }
     }
 }
